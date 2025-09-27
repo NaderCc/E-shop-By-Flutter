dde8b9b8eb55552aecfa11b6605176d390edbfea
NaderCc <kanmagdy@gmail.com>
2025-09-27 21:27:10 +0300

    Add Row of the catagories by pictures

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -60,6 +60,92 @@ class _HomePage extends State<HomePage>
 
                 margin: EdgeInsets.only(left: 30,top: 20),
                 child: Text("Catagories",textAlign: TextAlign.left,style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold,),),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+
+                        height: 100,
+                        width: 100,
+
+                        child:MaterialButton(onPressed: (){},
+                          child:ClipRRect(
+                            borderRadius: BorderRadius.circular(150),
+
+                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxm9XKblXXPwZIdwihN6dZZLGSzyHhQADIfA&s"
+                              ,fit: BoxFit.fill,),
+
+                          ) ,
+                        ),
+    ),
+                      Text("Men",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+
+                        height: 100,
+                        width: 100,
+
+                        child:MaterialButton(onPressed: (){},
+                          child:ClipRRect(
+                            borderRadius: BorderRadius.circular(150),
+
+                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd5Vu-wkLgpa4qhz9CyehrOZOmf48mVWTRcw&s"
+                              ,fit: BoxFit.fill,),
+
+                          ) ,
+                        ),
+                      ),
+                      Text("Women",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+
+                        height: 100,
+                        width: 100,
+
+                        child:MaterialButton(onPressed: (){},
+                          child:ClipRRect(
+                            borderRadius: BorderRadius.circular(150),
+
+                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzlSP0111Yyf2HscUDeU96eEqXT4FMsYjkcA&s"
+                              ,fit: BoxFit.fill,),
+
+                          ) ,
+                        ),
+                      ),
+                      Text("Electrical",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+
+                        height: 100,
+                        width: 100,
+
+                        child:MaterialButton(onPressed: (){},
+                          child:ClipRRect(
+                            borderRadius: BorderRadius.circular(150),
+
+                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4jEyesGSDgUo0fP6BtG5p1fWMMGOwOll8jA&s"
+                              ,fit: BoxFit.fill,),
+
+                          ) ,
+                        ),
+                      ),
+                      Text("Hobbies",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
+                    ],
+                  ),
+                  
+                ],
               )
 
             ],
