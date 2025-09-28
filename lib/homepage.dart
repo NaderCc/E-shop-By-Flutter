f14d04b3ff0038fa4c46418ecea2bbef647bc8c6
NaderCc <kanmagdy@gmail.com>
2025-09-28 17:42:38 +0300

    Add new widgets in Drawer

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -16,7 +17,34 @@ class _HomePage extends State<HomePage>
   Widget build(BuildContext context) {
     return  Scaffold(
       key: scaff,
-          drawer: Drawer(),
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                Center(child:Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  margin: EdgeInsets.only(top: 20,bottom: 15),
+                  height: 100,
+                  width: 100,
+
+                  child: Icon(Icons.person,size: 80,),
+                )),
+                Text("Hi Guest",textAlign: TextAlign.center,style: TextStyle(fontSize: 15)),
+                MaterialButton(onPressed: (){},
+                    color: Colors.white38,
+
+                  child:Text("Sign In",textAlign: TextAlign.center,style: TextStyle(fontSize: 20),)
+                ),
+                MaterialButton(onPressed: (){},
+                    color: Colors.white38,
+
+                    child:Text("Sign Up",textAlign: TextAlign.center,style: TextStyle(fontSize: 20),)
+                )
+              ],
+            ),
+          ),
 
           body: ListView(
 
@@ -61,93 +89,109 @@ class _HomePage extends State<HomePage>
                 margin: EdgeInsets.only(left: 30,top: 20),
                 child: Text("Catagories",textAlign: TextAlign.left,style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold,),),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      Container(
+              Container(
+                height: 150,
 
-                        height: 100,
-                        width: 100,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
 
-                        child:MaterialButton(onPressed: (){},
-                          child:ClipRRect(
-                            borderRadius: BorderRadius.circular(150),
+                          height: 100,
+                          width: 100,
 
-                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxm9XKblXXPwZIdwihN6dZZLGSzyHhQADIfA&s"
-                              ,fit: BoxFit.fill,),
+                          child:MaterialButton(onPressed: (){},
+                            child:ClipRRect(
+                              borderRadius: BorderRadius.circular(150),
 
-                          ) ,
+                              child: Image.asset("image/men.jpg"
+                                ,fit: BoxFit.fill,),
+
+                            ) ,
+                          ),
+                    ),
+                        Text("Men",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+
+                          height: 100,
+                          width: 100,
+
+                          child:MaterialButton(onPressed: (){},
+                            child:ClipRRect(
+                              borderRadius: BorderRadius.circular(150),
+
+                              child: Image.asset("image/women.jpg"
+                                ,fit: BoxFit.fill,),
+
+                            ) ,
+                          ),
                         ),
-    ),
-                      Text("Men",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
+                        Text("Women",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 100,
 
-                        height: 100,
-                        width: 100,
+                          child:MaterialButton(onPressed: (){},
+                            child:ClipRRect(
+                              borderRadius: BorderRadius.circular(150),
 
-                        child:MaterialButton(onPressed: (){},
-                          child:ClipRRect(
-                            borderRadius: BorderRadius.circular(150),
+                              child: Image.asset("image/ele.jpg"
+                                ,fit: BoxFit.fill,),
 
-                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd5Vu-wkLgpa4qhz9CyehrOZOmf48mVWTRcw&s"
-                              ,fit: BoxFit.fill,),
-
-                          ) ,
+                            ) ,
+                          ),
                         ),
-                      ),
-                      Text("Women",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
+                        Text("Electrical",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
 
-                        height: 100,
-                        width: 100,
+                          height: 100,
+                          width: 100,
 
-                        child:MaterialButton(onPressed: (){},
-                          child:ClipRRect(
-                            borderRadius: BorderRadius.circular(150),
+                          child:MaterialButton(onPressed: (){},
+                            child:ClipRRect(
+                              borderRadius: BorderRadius.circular(150),
 
-                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzlSP0111Yyf2HscUDeU96eEqXT4FMsYjkcA&s"
-                              ,fit: BoxFit.fill,),
+                              child: Image.asset("image/hop.png"
+                                ,fit: BoxFit.fill,),
 
-                          ) ,
+                            ) ,
+                          ),
                         ),
-                      ),
-                      Text("Electrical",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
+                        Text("Hobbies",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
+                      ],
+                    ),
 
-                        height: 100,
-                        width: 100,
+                  ],
+                ),
+              ),
+              Container(
 
-                        child:MaterialButton(onPressed: (){},
-                          child:ClipRRect(
-                            borderRadius: BorderRadius.circular(150),
+                margin: EdgeInsets.only(left: 30,top: 20),
+                child: Text("!! Best Selling !!",textAlign: TextAlign.left,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.red.shade200),),
+              ),
 
-                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4jEyesGSDgUo0fP6BtG5p1fWMMGOwOll8jA&s"
-                              ,fit: BoxFit.fill,),
+              GridView(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
 
-                          ) ,
-                        ),
-                      ),
-                      Text("Hobbies",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey)),
-                    ],
-                  ),
-                  
-                ],
-              )
+              shrinkWrap: true,
+              children: [
+                Card(
 
+                )
+              ],)
             ],
           )
           );
